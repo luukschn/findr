5e4281980547998b943e7df69239eb0cb71b82bd
luukschn <luuk.schneider@xs4all.nl>
2023-04-26 15:07:51 +0200

    test for conflicting functions

--- tests/Helpers/TestHelpers.php
+++ tests/Helpers/TestHelpers.php
@@ -5,22 +5,22 @@ use Tests\TestCase;
 class TestHelpers extends TestCase
 {
 
-    public function start_session($token) {
-        $session = $this->app->make('session');
-        $session->start();
-        $session->put('_token', $token);
-    }
+    // public function start_session($token) {
+    //     $session = $this->app->make('session');
+    //     $session->start();
+    //     $session->put('_token', $token);
+    // }
 
-    public function get_csrf_token() {
-        $response = $this->get('/register');
+    // public function get_csrf_token() {
+    //     $response = $this->get('/register');
 
-        $regex = '/<input type="hidden" name="_token" value="(.+)"/';
-        preg_match($regex, $response->getContent(), $matches);
-        $token = $matches[0];
+    //     $regex = '/<input type="hidden" name="_token" value="(.+)"/';
+    //     preg_match($regex, $response->getContent(), $matches);
+    //     $token = $matches[0];
 
-        TestHelpers::start_session($token);
+    //     TestHelpers::start_session($token);
 
-        return $token;
-    }
+    //     return $token;
+    // }
 
 }
